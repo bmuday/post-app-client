0c773432dd46b3a8ee4efa6227a4933685819135
bmuday <bmuday@live.fr>
2021-06-15 20:08:35 +0200

    change origin url axios

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
     };
 
     axios({
-      url: "http://104.248.234.175:5000/api/save",
+      url: "/api/save",
       method: "POST",
       data: payload,
     })
@@ -45,7 +45,7 @@ const App = () => {
 
   const getPosts = () => {
     axios({
-      url: "http://104.248.234.175:5000/api",
+      url: "/api",
     })
       .then((response) => {
         const data = response.data;
